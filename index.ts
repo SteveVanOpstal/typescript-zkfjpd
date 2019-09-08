072b1375db6b5aa13e9834e75b330a07693271a4
SteveVanOpstal <stevevanopstal@gmail.com>
2019-09-08 12:59:48 +0200

    1 binaries uploaded

--- index.ts
+++ index.ts
@@ -2,11 +2,21 @@
 import Jimp from 'jimp/es';
 
 Jimp.read('test.png').then((image) => {
-  image.resize(size, Jimp.AUTO)
+  image.resize(1440, Jimp.AUTO)
       .quality(100)
       .getBufferAsync(Jimp.MIME_JPEG)
-      .then((d: Buffer) => covers.push({size: size, data: d}));
+      .then((d: Buffer) => {
+        const img: HTMLImageElement = document.getElementById('not_resized');
+        img.src = d;
+      });
 });
 
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<img src=""/>`;
+Jimp.read('test.png').then((image) => {
+  image.resize(1440, Jimp.AUTO)
+      .quality(100)
+      .getBufferAsync(Jimp.MIME_JPEG)
+      .then((d: Buffer) => {
+        const img: HTMLImageElement = document.getElementById('not_resized');
+        img.src = d;
+      });
+});
